7fcd198e4ca01720f713385e993ded1534f5dcf4
pavanjoy2002 <pavansky2002@gmail.com>
2024-02-01 20:33:41 +0530

    add

---
+++ add.java
@@ -0,0 +1,18 @@
+package obj;
+
+public class add {
+	public static void main(String[] args) 
+	{
+	    int x = 0;
+	    while(x<=3)
+	    {
+	    
+	         System.out.println("the number is "+x);
+	         x++;
+	    
+	    }
+	    
+	  }
+	}
+
+
